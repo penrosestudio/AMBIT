012fb8715300da1939102bb7173f66fe27e8ea77
Chris Sugden <csugden@gmail.com>
2012-06-12 14:30:14 +0100

    Alters Provenance feature to show only local/derived label, and to not use popups

--- v3/theme/ProvenanceMacro.js
+++ v3/theme/ProvenanceMacro.js
@@ -2,34 +2,25 @@
 config.macros.provenance = {
 	handler: function(place,macroName,params,wikifier,paramString,tiddler) {
 		var $ = jQuery;
-		var popUpText;
 		var currentSpace = config.extensions.tiddlyspace.currentSpace.name;
-		
 		var bag = tiddler.fields['server.bag'];
 		var tiddlerSpace = bag.substring(0,bag.lastIndexOf('_'));		
-		
 		var isLocal = config.filterHelpers.is.local(tiddler);
-
 		var source = tiddler.fields['tiddler.source'];
+		var labelText;
 
 		if (isLocal) {
 			if (source) {
-				popUpText = "This page is an overwrite of <a href='http://" + source + ".tiddlyspace.com/#[[" + encodeURIComponent(tiddler.title) + "]]' target='_blank'>" + source + "/" + tiddler.title + "</a>";
+				labelText = "Derived from <a href='http://" + source + ".tiddlyspace.com/#[[" + encodeURIComponent(tiddler.title) + "]]' target='_blank'>" + source + "</a>";
 			} else {
-				popUpText = "This is a local " + currentSpace + " page.";
+				labelText = currentSpace + " original content.";
 			}
 		} else {
-			popUpText = "This page is inherited from <a href='http://" + tiddlerSpace + ".tiddlyspace.com/#[[" + encodeURIComponent(tiddler.title) + "]]' target='_blank'>" + tiddlerSpace + "</a>";
+			// Page is inherited. Return without adding label. 
+			return;
 		}
 
-		$(place).html(tiddlerSpace);
-		
-		$(place).click(function() {
-			var popup = Popup.create(this);
-			$(popup).append(popUpText);
-			Popup.show();
-			return false;
-		});
+		$(place).html(labelText);
 	}
 };
 /*}}}*/
