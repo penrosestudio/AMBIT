362b67188329486770aa3c789049a8a0601bc68c
Chris Sugden <csugden@gmail.com>
2012-06-26 09:46:59 +0100

    Alters Provenance macro to fix timing bug experienced when cloning

--- v3/theme/ProvenanceMacro.js
+++ v3/theme/ProvenanceMacro.js
@@ -1,14 +1,17 @@
 /*{{{*/
 config.macros.provenance = {
-	handler: function(place,macroName,params,wikifier,paramString,tiddler) {
+	/* Provenance macro checks the origin of the tiddler.
+	   If it is inherited from another space, nothing is done.
+	   If it was created in this space, or is a local modification
+	   of a tiddler from another space, a label indicating this is
+	   added.
+	*/
+  provenanceHelper: function(place, tiddler) {
 		var $ = jQuery;
 		var currentSpace = config.extensions.tiddlyspace.currentSpace.name;
-		var bag = tiddler.fields['server.bag'];
-		var tiddlerSpace = bag.substring(0,bag.lastIndexOf('_'));		
-		var isLocal = config.filterHelpers.is.local(tiddler);
 		var source = tiddler.fields['tiddler.source'];
 		var labelText;
-
+		var isLocal = config.filterHelpers.is.local(tiddler);
 		if (isLocal) {
 			if (source) {
 				labelText = "Derived from <a href='http://" + source + ".tiddlyspace.com/#[[" + encodeURIComponent(tiddler.title) + "]]' target='_blank'>" + source + "</a>";
@@ -16,11 +19,20 @@ config.macros.provenance = {
 				labelText = currentSpace + " original content.";
 			}
 		} else {
-			// Page is inherited. Return without adding label. 
+			// Page is inherited. Return without adding label.
 			return;
 		}
-
-		$(place).html(labelText);
+		$(place).html(labelText);  	
+  },
+	handler: function(place,macroName,params,wikifier,paramString,tiddler) {
+		/* Run provenanceHelper immediately, and then again after a delay
+		   to ensure that server-side delays do not prevent the macro
+		   from displaying the correct information for a newly cloned tiddler.
+		*/
+		provenanceMacro.provenanceHelper(place, tiddler);
+		setTimeout(function(){
+			provenanceMacro.provenanceHelper(place, tiddler);
+		}, 2000);
 	}
 };
 /*}}}*/
