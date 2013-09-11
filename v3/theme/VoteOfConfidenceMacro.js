5c8366e9f8f0aff4845f72717a0af166c7e7835f
jayfresh <jonathan@penrosestudio.com>
2013-09-11 19:58:12 +0100

    deploying working version; will need to uncomment the cloning section of VoteOfConfidenceMacro once it's approved for deployment

--- v3/theme/VoteOfConfidenceMacro.js
+++ v3/theme/VoteOfConfidenceMacro.js
@@ -8,17 +8,32 @@
 		- when you click on the count, open up a popup with the information:
 			- title of tiddler / space / date cloned
 
+	BUG: if the tiddler loads visible (e.g. by visiting that previous link), this doesn't work. See Provenance macro for help I think! The view needs to be refreshed
+	
+	On cloning, what I want to be able to support is this flow:
+		- I clone a tiddler and save it in my space
+		- when I visit the space I cloned the tiddler from, the tiddler I cloned tells me that clones of it exist, and points to which spaces they are in and when they were cloned
+		- I want this connection to remain EVEN IF I change the title of the tiddler in my space
+		(nice to have: - I want this connection to remain EVEN IF the title of the tiddler I cloned changes)
+
+	pieces:
+		- <bag>/<title> _source field e.g. _source: /bags/totw_public/tiddlers/Why
+		- preserving original title in a field when renaming (?) You could presumably alter the rename routines to keep a
+history of previous names
+			- don't do this for now
+		- TwinTiddlers (?)
+
 */
 
 var voteOfConfidenceMacro = config.macros.voteOfConfidence = {
 	/* Vote of Confidence macro checks how many times a tiddler has been cloned and display some information about those clones. */
-	clonedTiddlers: [],
+	clonedTiddlers: {},
 	init: function() {
 		var plugin = config.macros.voteOfConfidence,
 			$ = jQuery,
 			currentSpace = config.extensions.tiddlyspace.currentSpace.name,
-			//url = "/search?q=tiddler.source:"+currentSpace+"_public";
-			url = "/search?q=tiddler.source:ambit-content_public"; // this is for debug
+			url = "/search.json?q=tiddler.source:"+currentSpace+"_public";
+		url = "/search.json?q=tiddler.source:"+currentSpace+"_public"; // this is for debug
 		$.ajax({
 			url: url,
 			dataType: "json",
@@ -27,13 +42,16 @@ var voteOfConfidenceMacro = config.macros.voteOfConfidence = {
 				tiddlers = $.grep(tiddlers, function(t, i) {
 					return t.bag.indexOf('ambit')!==-1;
 				});
-				// index all the cloned tiddlers by title
+				// index all the cloned tiddlers by _source
 				$.each(tiddlers, function(i, tiddler) {
-					var title = tiddler.title;
-					if(!plugin.clonedTiddlers[title]) {
-						plugin.clonedTiddlers[title] = [];
+					var _source = tiddler.fields['tiddler._source'];
+					if(!_source) {
+						return true;
 					}
-					plugin.clonedTiddlers[title].push(tiddler);
+					if(!plugin.clonedTiddlers[_source]) {
+						plugin.clonedTiddlers[_source] = [];
+					}
+					plugin.clonedTiddlers[_source].push(tiddler);
 				});
 			},
 			error: function() {
@@ -41,14 +59,18 @@ var voteOfConfidenceMacro = config.macros.voteOfConfidence = {
 			}
 		});
 	},
+	getSourceFromTitle: function(title,space) {
+		var currentSpace = config.extensions.tiddlyspace.currentSpace.name;
+		return '/bags/'+(currentSpace||space)+'_public/tiddlers/'+encodeURIComponent(title);
+	},
 	handler: function(place,macroName,params,wikifier,paramString,tiddler) {
 		var plugin = config.macros.voteOfConfidence,
 			$ = jQuery,
 			$place = $(place),
 			title = tiddler.title,
-			clones = plugin.clonedTiddlers[title],
+			_source = plugin.getSourceFromTitle(title),
+			clones = plugin.clonedTiddlers[_source],
 			clonedCount = clones && clones.length;
-		console.log(clones);
 		if(!clonedCount) {
 			return;
 		}
@@ -56,11 +78,13 @@ var voteOfConfidenceMacro = config.macros.voteOfConfidence = {
 			.children('a')
 			.click(function(e) {
 				e.preventDefault();
-				var popup = Popup.create(this),
+				var popup = Popup.create(this,null,"voteOfConfidence popup"),
 					$popup = $(popup),
 					htmlPieces = ["<div>"];
 				$.each(clones, function(i, t) {
-					htmlPieces.push("<div class='grid2col left'>"+t.title+"</div><div class='grid2col left'>"+t.bag+"</div><div class='grid2col left'>"+t.created+"</div><br class='clearboth' />");
+					var space = t.bag.replace('_public',''),
+						created = Date.convertFromYYYYMMDDHHMM(t.created).formatString('0DD/0MM/YY');
+					htmlPieces.push("<div class='grid2col left marginright'><a href='http://"+space+".tiddlyspace.com/#[["+t.title+"]]' target='_blank'>"+t.title+"</a></div><div class='grid2col left marginright'>"+space+"</div><div class='grid2col left'>"+created+"</div><br class='clearboth' />");
 				});
 				htmlPieces.push("</div>");
 				html = htmlPieces.join("\n");
@@ -70,4 +94,20 @@ var voteOfConfidenceMacro = config.macros.voteOfConfidence = {
 			});
 	}
 };
+
+/* addition to cloneTiddler so that it saves the cloned tiddler title as _source field */
+/*var _cloneHandler = config.commands.cloneTiddler.handler;
+config.commands.cloneTiddler.handler = function(event, src, title) {
+	var _tiddler = store.getTiddler(title);
+	var source = _tiddler ? _tiddler.fields["server.bag"] : false;
+	var _source = _tiddler ? '/bags/'+source+'/tiddlers/'+encodeURIComponent(_tiddler.title) : false;
+	var realTitle = _tiddler ? _tiddler.fields["server.title"] : title;
+	_cloneHandler.apply(this, [event, src, title]);
+	var tidEl = story.getTiddler(title);
+	$(story.getTiddlerField(title, "title")).val(realTitle);
+	if(_source) {
+		$("<input />").attr("type", "hidden").attr("edit", "tiddler._source").val(_source).appendTo(tidEl);
+	}
+};*/
+
 /*}}}*/
