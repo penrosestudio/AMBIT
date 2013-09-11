a449e1aa29a91b14da98b08644fd349f4e91c5ba
jayfresh <jonathan@penrosestudio.com>
2013-09-11 20:20:23 +0100

    reworked VoC label display so it always happens after the AJAX request has completed

--- v3/theme/VoteOfConfidenceMacro.js
+++ v3/theme/VoteOfConfidenceMacro.js
@@ -28,6 +28,7 @@ history of previous names
 var voteOfConfidenceMacro = config.macros.voteOfConfidence = {
 	/* Vote of Confidence macro checks how many times a tiddler has been cloned and display some information about those clones. */
 	clonedTiddlers: {},
+	queue: [],
 	init: function() {
 		var plugin = config.macros.voteOfConfidence,
 			$ = jQuery,
@@ -38,6 +39,7 @@ var voteOfConfidenceMacro = config.macros.voteOfConfidence = {
 			url: url,
 			dataType: "json",
 			success: function(tiddlers) {
+				plugin.initDone = true;
 				// make sure we are not including any non-ambit tiddlers
 				tiddlers = $.grep(tiddlers, function(t, i) {
 					return t.bag.indexOf('ambit')!==-1;
@@ -53,6 +55,10 @@ var voteOfConfidenceMacro = config.macros.voteOfConfidence = {
 					}
 					plugin.clonedTiddlers[_source].push(tiddler);
 				});
+				// trigger any waiting macro calls
+				$.each(plugin.queue, function() {
+					this();
+				});
 			},
 			error: function() {
 				// TO-DO: decide what to do in the event of an error
@@ -64,34 +70,45 @@ var voteOfConfidenceMacro = config.macros.voteOfConfidence = {
 		return '/bags/'+(currentSpace||space)+'_public/tiddlers/'+encodeURIComponent(title);
 	},
 	handler: function(place,macroName,params,wikifier,paramString,tiddler) {
+		console.log('handler');
 		var plugin = config.macros.voteOfConfidence,
-			$ = jQuery,
-			$place = $(place),
-			title = tiddler.title,
-			_source = plugin.getSourceFromTitle(title),
-			clones = plugin.clonedTiddlers[_source],
-			clonedCount = clones && clones.length;
-		if(!clonedCount) {
-			return;
+			displayCloneCount = function() {
+				var plugin = config.macros.voteOfConfidence,
+					$ = jQuery,
+					$place = $(place),
+					title = tiddler.title,
+					_source = plugin.getSourceFromTitle(title),
+					clones = plugin.clonedTiddlers[_source],
+					clonedCount = clones && clones.length;
+				if(!clonedCount) {
+					return;
+				}
+				$place.html('<a href="#">cloned '+clonedCount+' times</a>')
+					.children('a')
+					.click(function(e) {
+						e.preventDefault();
+						var popup = Popup.create(this,null,"voteOfConfidence popup"),
+							$popup = $(popup),
+							htmlPieces = ["<div>"];
+						$.each(clones, function(i, t) {
+							var space = t.bag.replace('_public',''),
+								created = Date.convertFromYYYYMMDDHHMM(t.created).formatString('0DD/0MM/YY');
+							htmlPieces.push("<div class='grid2col left marginright'><a href='http://"+space+".tiddlyspace.com/#[["+t.title+"]]' target='_blank'>"+t.title+"</a></div><div class='grid2col left marginright'>"+space+"</div><div class='grid2col left'>"+created+"</div><br class='clearboth' />");
+						});
+						htmlPieces.push("</div>");
+						html = htmlPieces.join("\n");
+						$(html).appendTo($popup);
+						Popup.show();
+						return false;
+					});
+			};
+		if(!plugin.initDone) {
+			// place this call into a queue
+			plugin.queue.push(displayCloneCount);
+		} else {
+			// perform this call now
+			displayCloneCount();
 		}
-		$place.html('<a href="#">cloned '+clonedCount+' times</a>')
-			.children('a')
-			.click(function(e) {
-				e.preventDefault();
-				var popup = Popup.create(this,null,"voteOfConfidence popup"),
-					$popup = $(popup),
-					htmlPieces = ["<div>"];
-				$.each(clones, function(i, t) {
-					var space = t.bag.replace('_public',''),
-						created = Date.convertFromYYYYMMDDHHMM(t.created).formatString('0DD/0MM/YY');
-					htmlPieces.push("<div class='grid2col left marginright'><a href='http://"+space+".tiddlyspace.com/#[["+t.title+"]]' target='_blank'>"+t.title+"</a></div><div class='grid2col left marginright'>"+space+"</div><div class='grid2col left'>"+created+"</div><br class='clearboth' />");
-				});
-				htmlPieces.push("</div>");
-				html = htmlPieces.join("\n");
-				$(html).appendTo($popup);
-				Popup.show();
-				return false;
-			});
 	}
 };
 
